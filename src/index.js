212511e9e55556cd45e4cf6edf729f16a86a4e4d
Jason <me@jasonyuan.design>
2018-06-20 21:29:31 -0700

    im not cocky i just love myselfs

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import {Motion, spring} from 'react-motion';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
